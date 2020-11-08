a22d725db13dd1c6496bc273c5a0bb30073a1d08
Pedro Fillastre <pedrogas_g@hotmail.com>
2020-11-08 21:13:09 +0100

    Finish speed setters

--- two_wheel_simulation/src/two_wheel_plugin.cpp
+++ two_wheel_simulation/src/two_wheel_plugin.cpp
@@ -13,20 +13,63 @@ namespace gazebo_plugins
 {
   class TwoWheelPluginPrivate
   {
-  public:
-    void OnLoad(const gazebo::physics::ModelPtr _model);
+  private:
     gazebo_ros::Node::SharedPtr ros_node_;
-    gazebo::physics::ModelPtr model_;
+    gazebo::physics::JointControllerPtr controller_;
     gazebo::physics::JointPtr left_joint_;
-    gazebo::common::PID left_pid_;
+    gazebo::physics::JointPtr right_joint_;
+    gazebo::physics::ModelPtr model_;
+    sdf::ElementPtr sdf_;
 
+  public:
+    void OnLoad(const gazebo::physics::ModelPtr _model, sdf::ElementPtr _sdf);
+    void InitializeLeftJoint();
+    void InitializeRightJoint();
+    void SetLeftVelocity(double speed);
+    void SetRightVelocity(double speed);
+  
+  private:
+    gazebo::physics::JointPtr InitializeJoint(std::string _joint_name);
   };
 
-  void TwoWheelPluginPrivate::OnLoad(const gazebo::physics::ModelPtr _model)
+  void TwoWheelPluginPrivate::OnLoad(const gazebo::physics::ModelPtr _model,  sdf::ElementPtr _sdf)
   {
+    model_ = _model;
+    sdf_= _sdf;
+    ros_node_ = gazebo_ros::Node::Get(_sdf);
+    controller_ = _model->GetJointController();
     std::cerr << "\nThe velodyne plugin is attach to model[" <<
         _model->GetName() << "]\n";
     RCLCPP_WARN(this->ros_node_->get_logger(), "\n The plugin is working in the model : \n", _model->GetName());
+  }
+
+  gazebo::physics::JointPtr TwoWheelPluginPrivate::InitializeJoint(std::string _joint_name)
+  {
+    auto joint_name = sdf_->GetElement(_joint_name)->Get<std::string>();
+    auto joint = model_->GetJoint(joint_name);
+    auto pid = gazebo::common::PID(0.1, 0, 0);
+    controller_->SetVelocityPID(joint->GetScopedName(), pid);
+    return joint;
+  }
+
+  void TwoWheelPluginPrivate::InitializeLeftJoint()
+  {
+    left_joint_ = InitializeJoint("left_joint");
+  }
+
+  void TwoWheelPluginPrivate::InitializeRightJoint()
+  {
+    right_joint_ = InitializeJoint("right_joint");
+  }
+
+  void TwoWheelPluginPrivate::SetLeftVelocity(double speed)
+  {
+    controller_->SetVelocityTarget(left_joint_->GetScopedName(), speed);
+  }
+  
+  void TwoWheelPluginPrivate::SetRightVelocity(double speed)
+  {
+    controller_->SetVelocityTarget(right_joint_->GetScopedName(), speed);
   }
 
   TwoWheelPlugin::TwoWheelPlugin(/* args */)
@@ -40,17 +83,13 @@ namespace gazebo_plugins
 
   void TwoWheelPlugin::Load(gazebo::physics::ModelPtr _model, sdf::ElementPtr _sdf)
   {
-    impl_->ros_node_ = gazebo_ros::Node::Get(_sdf);
-    impl_->model_ = _model;
-    auto left_joint_name = _sdf->GetElement("left_joint")->Get<std::string>();
-    impl_->left_joint_ = _model->GetJoint(left_joint_name);
-    impl_->left_pid_ = gazebo::common::PID(0.1, 0, 0);
-    auto controller = impl_->model_->GetJointController();
-    controller->SetVelocityPID(impl_->left_joint_->GetScopedName(), impl_->left_pid_);
-    controller->SetVelocityTarget(impl_->left_joint_->GetScopedName(), 10.0);
-    impl_->OnLoad(_model);
-
+    impl_->OnLoad(_model, _sdf);
+    impl_->InitializeLeftJoint();
+    impl_->InitializeRightJoint();
+    impl_->SetLeftVelocity(10.0);
+    impl_->SetRightVelocity(-10.0);
     
+  
   }
 
   GZ_REGISTER_MODEL_PLUGIN(TwoWheelPlugin)
