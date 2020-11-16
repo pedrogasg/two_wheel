c4de895d5e336f03bdd958865b5d691eef69f2aa
Pedro Fillastre <pedrogas_g@hotmail.com>
2020-11-16 01:22:51 +0100

    Differential drive

--- two_wheel_simulation/src/two_wheel_plugin.cpp
+++ two_wheel_simulation/src/two_wheel_plugin.cpp
@@ -1,11 +1,16 @@
 #include <two_wheel_simulation/two_wheel_plugin.hpp>
+
 #include <gazebo/physics/JointController.hh>
 #include <gazebo/physics/Model.hh>
 #include <gazebo/physics/Joint.hh>
 #include <gazebo/common/PID.hh>
-#include <gazebo_ros/node.hpp>
-#include <rclcpp/rclcpp.hpp>
 #include <sdf/sdf.hh>
+
+#include <gazebo_ros/node.hpp>
+
+#include <geometry_msgs/msg/twist.hpp>
+#include <rclcpp/rclcpp.hpp>
+
 #include <cstdio>
 #include <string>
 
@@ -21,12 +26,25 @@ namespace gazebo_plugins
     gazebo::physics::ModelPtr model_;
     sdf::ElementPtr sdf_;
 
+    double wheel_radius_{10.0};
+
+    double base_length_{10.0};
+    
+    /// Linear velocity in X received on command (m/s).
+    double target_v_{0.0};
+
+    /// Angular velocity in Z received on command (rad/s).
+    double target_w_{0.0};
+
+    std::mutex lock_;
+
   public:
     void OnLoad(const gazebo::physics::ModelPtr _model, sdf::ElementPtr _sdf);
     void InitializeLeftJoint();
     void InitializeRightJoint();
     void SetLeftVelocity(double speed);
     void SetRightVelocity(double speed);
+    void OnCmdVel(const geometry_msgs::msg::Twist::SharedPtr _msg);
   
   private:
     gazebo::physics::JointPtr InitializeJoint(std::string _joint_name);
@@ -38,6 +56,9 @@ namespace gazebo_plugins
     sdf_= _sdf;
     ros_node_ = gazebo_ros::Node::Get(_sdf);
     controller_ = _model->GetJointController();
+    wheel_radius_ = sdf_->GetElement("wheel_radius")->Get<double>();
+    base_length_ = sdf_->GetElement("base_length")->Get<double>();
+    
     std::cerr << "\nThe velodyne plugin is attach to model[" <<
         _model->GetName() << "]\n";
     RCLCPP_WARN(this->ros_node_->get_logger(), "\n The plugin is working in the model : \n", _model->GetName());
@@ -72,6 +93,18 @@ namespace gazebo_plugins
     controller_->SetVelocityTarget(right_joint_->GetScopedName(), speed);
   }
 
+  void TwoWheelPluginPrivate::OnCmdVel(const geometry_msgs::msg::Twist::SharedPtr _msg)
+  {
+    std::lock_guard<std::mutex> scoped_lock(lock_);
+    target_v_ = _msg->linear.x;
+    target_w_ = _msg->angular.z;
+    auto R_2 = wheel_radius_ * 2;
+    auto V_2 = target_v_ * 2;
+    auto L_w = base_length_ * target_w_;
+    this->SetLeftVelocity((V_2 - L_w)/R_2);
+    this->SetRightVelocity((V_2 + L_w)/R_2);
+  }
+
   TwoWheelPlugin::TwoWheelPlugin(/* args */)
     : impl_(std::make_unique<TwoWheelPluginPrivate>())
   {
@@ -86,10 +119,6 @@ namespace gazebo_plugins
     impl_->OnLoad(_model, _sdf);
     impl_->InitializeLeftJoint();
     impl_->InitializeRightJoint();
-    impl_->SetLeftVelocity(10.0);
-    impl_->SetRightVelocity(-10.0);
-    
-  
   }
 
   GZ_REGISTER_MODEL_PLUGIN(TwoWheelPlugin)
